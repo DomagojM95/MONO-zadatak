22b5aefeb0bafb5505d1f11477ea0d5e0e1b2a5a
DomagojM95 <markovicdomagoj1995@gmail.com>
2023-12-08 07:21:43 +0100

    Initial commit

---
+++ SQL Baza/SQL Baza.sql
@@ -0,0 +1,37 @@
+use master;
+
+drop database if exists vehiclecatalog;
+go
+
+create database vehiclecatalog;
+go
+use vehiclecatalog;
+
+create table VehicleMake(
+		ID int not null primary key identity,
+		carname varchar(50) not null,
+		abrv varchar(50)
+
+
+);
+
+create table VehicleModel(
+		ID int not null primary key identity,
+		VehicleMake int,
+		carname varchar(50) not null,
+		abrv varchar(50)
+
+
+);
+
+alter table VehicleModel add foreign key (VehicleMake) references VehicleMake(ID);
+
+insert into VehicleMake(carname,abrv)
+values('Toyota','Tyt');
+
+select * from VehicleMake;
+
+insert into VehicleModel(VehicleMake,carname,abrv)
+values(1,'Toyota','Tyt');
+
+select * from VehicleModel;
